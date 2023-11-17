c56722dd21c2a03b4a2fcd2bf5b166b57cc55247
Kazi_Afroz <k.afroz.bd@gmail.com>
2023-11-17 21:23:29 +0600

    relative + absolute path & rename file

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -11,28 +12,45 @@ func main() {
 	if err!=nil {
 		fmt.Println(err.Error())
 	}
-	fmt.Println(dir)
+// 	fmt.Println(dir)
 
-	isErr := fileCreate("test.txt", "test 2: hi hello..")
-	fmt.Println(isErr)
+// 	isErr := fileCreate("test.txt", "test 2: hi hello..")
+// 	fmt.Println(isErr)
 
 
-	fi, err := os.Stat("ami.txt")
-	if err!=nil {
-		fmt.Println(err.Error())
-	}
+// 	fi, err := os.Stat("ami.txt")
+// 	if err!=nil {
+// 		fmt.Println(err.Error())
+// 	}
 
-	fmt.Println(fi.IsDir())
-	fmt.Println(fi.ModTime())
-	fmt.Println(fi.Name())
-	fmt.Println(fi.Size())
+// 	fmt.Println(fi.IsDir())
+// 	fmt.Println(fi.ModTime())
+// 	fmt.Println(fi.Name())
+// 	fmt.Println(fi.Size())
 
-//create folder
-	err = os.Mkdir("fileHandleFolder", 0777)
-	if err!=nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(err)
+// //create folder
+// 	err = os.Mkdir("fileHandleFolder", 0777)
+// 	if err!=nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	fmt.Println(err)
+
+	base := filepath.Base(dir)
+	fmt.Println(base)
+	relativePath:=filepath.Join("fileHandleFolder")
+	fmt.Println(relativePath)
+	absolutePath,_ := filepath.Abs("fileHandleFolder")
+	fmt.Println(absolutePath)
+
+	os.Rename("fileHandleFolder","folder_new111")
+
+	newPath := filepath.Join(absolutePath,"..","..", "folder_new")
+	fmt.Println(newPath)
+
+	os.Mkdir(newPath, 777)
+
+	
+
 
 }
 
