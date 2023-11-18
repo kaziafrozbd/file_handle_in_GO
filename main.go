5d916a895bbcaa74cabe2446e16b5e9484d69535
Kazi_Afroz <k.afroz.bd@gmail.com>
2023-11-18 12:21:43 +0600

    excel file create and write

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
+	// "os"
+	// "path/filepath"
+
+	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	//show directory of current file
-	dir, err := os.Getwd()
-	if err!=nil {
-		fmt.Println(err.Error())
-	}
+	// //show directory of current file
+	// dir, err := os.Getwd()
+	// if err!=nil {
+	// 	fmt.Println(err.Error())
+	// }
 // 	fmt.Println(dir)
 
 // 	isErr := fileCreate("test.txt", "test 2: hi hello..")
@@ -35,39 +37,59 @@ func main() {
 // 	}
 // 	fmt.Println(err)
 
-	base := filepath.Base(dir)
-	fmt.Println(base)
-	relativePath:=filepath.Join("fileHandleFolder")
-	fmt.Println(relativePath)
-	absolutePath,_ := filepath.Abs("fileHandleFolder")
-	fmt.Println(absolutePath)
+	// base := filepath.Base(dir)
+	// fmt.Println(base)
+	// relativePath:=filepath.Join("fileHandleFolder")
+	// fmt.Println(relativePath)
+	// absolutePath,_ := filepath.Abs("fileHandleFolder")
+	// fmt.Println(absolutePath)
 
-	os.Rename("fileHandleFolder","folder_new111")
+	// os.Rename("fileHandleFolder","folder_new111")
 
-	newPath := filepath.Join(absolutePath,"..","..", "folder_new")
-	fmt.Println(newPath)
+	// newPath := filepath.Join(absolutePath,"..","..", "folder_new")
+	// fmt.Println(newPath)
 
-	os.Mkdir(newPath, 777)
+	// os.Mkdir(newPath, 777)
 
 	
-
+	f := excelize.NewFile()
+    defer func() {
+        if err := f.Close(); err != nil {
+            fmt.Println(err)
+        }
+    }()
+    // Create a new sheet.
+    index, err := f.NewSheet("Sheet2")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    // Set value of a cell.
+    f.SetCellValue("Sheet2", "A2", "Hello world.")
+    f.SetCellValue("Sheet1", "B2", 100)
+    // Set active sheet of the workbook.
+    f.SetActiveSheet(index)
+    // Save spreadsheet by the given path.
+    if err := f.SaveAs("Book1.xlsx"); err != nil {
+        fmt.Println(err)
+    }
 
 }
 
-func fileCreate(fineName,content string) bool {
-	//create directory
-	posf,err := os.Create(fineName)
-	if err!=nil {
-		fmt.Println(err.Error())
-	}
-	defer posf.Close()
+// func fileCreate(fineName,content string) bool {
+// 	//create directory
+// 	posf,err := os.Create(fineName)
+// 	if err!=nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	defer posf.Close()
 
-	//write in directory
-	_ ,err = posf.Write([]byte(content))
-	// fmt.Println(n)
-	if err!=nil {
-		fmt.Println(err.Error())
-		return false
-	}
-	return true
-}
+// 	//write in directory
+// 	_ ,err = posf.Write([]byte(content))
+// 	// fmt.Println(n)
+// 	if err!=nil {
+// 		fmt.Println(err.Error())
+// 		return false
+// 	}
+// 	return true
+// }
